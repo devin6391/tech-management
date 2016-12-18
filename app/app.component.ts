3fe9a1baa555be6b9f4a7470c03a7d4351add22e
Vineet Dev <vineetdev2008@gmail.com>
2016-12-19 02:30:15 +0530

    basic angular app setup
    Signed-off-by: Vineet Dev <vineetdev2008@gmail.com>

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Angular'; }
